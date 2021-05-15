c9a1c700e11efd2f010d7bd34bb15172c23cd459
Nidhi Raj Patel <59306056+nid1234-ni@users.noreply.github.com>
2021-05-16 00:36:30 +0530

    Add files via upload

---
+++ Bagofwords.py
@@ -0,0 +1,35 @@
+import nltk
+
+
+paragraph = """In 3000 years of our history, people from all over the world have come and invaded us, captured our lands, conquered our minds. From Alexander onwards, the Greeks, the Turks, the Moguls, the Portuguese, the British, the French, the Dutch, all of them came and looted us, took over what was ours. Yet, we have not done this to any other nation. We have not conquered anyone. We have not grabbed their land, their culture, their history and tried to enforce our way of life on them. Why? Because we respect the freedom of others.
+
+That is why my first vision is that of FREEDOM.
+
+I believe that India got its first vision of this in 1857, when we started the war of Independence. It is this freedom that we must protect and nurture and build on. If we are not free, no one will respect us.
+
+My second vision for India’s DEVELOPMENT.
+
+For fifty years we have been a developing nation. It is time we see ourselves as a developed nation. We are among top five nations of the world in terms of GDP. We have 10 per cent growth rate in most areas. Our poverty levels are falling. Our achievements are being globally recognised today. Yet we lack the self-confidence to see ourselves as a developed nation,
+ self-reliant and self-assured. Isn’t this incorrect"""
+
+import re
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+
+ps = PorterStemmer()
+Wordnet = WordNetLemmatizer()
+sentences = nltk.sent_tokenize(paragraph)
+corpus = []
+
+for i in range(len(sentences)):
+    review = re.sub('[^a-zA-Z]', ' ',sentences[i])
+    review = review.lower()
+    review = review.split()
+    review = [ps.stem(word) for word in review if not word in set(stopwords('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+    
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features=1500)
+X = cv.transform(corpus).toarray()
